70fd9481ef1c8b60c8653575655266e8ce6f1461
Anjana-20782 <anjanasivadasan0305@gmail.com>
2026-01-07 16:02:53 +0530

    state and eventhandler addad

--- State&Event Handling/src/App.jsx
+++ State&Event Handling/src/App.jsx
@@ -2,17 +2,19 @@ import React from "react";
 import Counter from "./components/Counter";
 import InputDemo from "./components/InputFiledUpdate";
 import ToggleDiv from "./components/ToggleVisibility";
+import List from "./components/EventHnadler";
 
-function App(){
+function App() {
+  const colors = ["Red", "Green", "Blue"];
 
-    return(
-
-        <div>
-            <Counter /><br /><br />
-            <InputDemo />
-            <ToggleDiv />
-        </div>
-    )
+  return (
+    <div>
+      <Counter /><br /><br />
+      <InputDemo />
+      <ToggleDiv /><br></br>
+      <List items={colors} />
+    </div>
+  );
 }
 
 export default App;
