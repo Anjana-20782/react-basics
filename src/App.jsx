ded5fe9b351be1535d96d15e76384729021c3a6b
Anjana-20782 <anjanasivadasan0305@gmail.com>
2026-01-07 14:45:14 +0530

    react-basics greeting display

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Hello from "./components/Hello"
+import Greeting from "./components/Greeting"
 
 function App(){
   return(
     <div>
       <Hello />
+      <Greeting name="John" />
+        <Greeting name="Alice" />
     </div>
   )
 }
