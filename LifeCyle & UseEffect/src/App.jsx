203046b24e8a4ccafe2d38e48b9914ae87ecd6cd
Anjana-20782 <anjanasivadasan0305@gmail.com>
2026-01-08 09:44:06 +0530

    lifecycle and useeffect mounted added

---
+++ LifeCyle & UseEffect/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ComponentDidMounted from "./components/Mounted";
+
+
+function App(){
+
+  return(
+
+    <div>
+      <ComponentDidMounted />
+    </div>
+  )
+}
+
+export default App;
---
+++ LifeCyle & UseEffect/src/components/Mounted.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function ComponentDidMounted(){
+
+    console.log("Mounted");
+    
+}
+
+export default ComponentDidMounted;
