60bc4b01b8a366f4da76af5792419053d546fada
Anjana-20782 <anjanasivadasan0305@gmail.com>
2026-01-08 09:48:05 +0530

    lifecycle and useeffect mounted added

--- LifeCyle & UseEffect/src/components/Mounted.jsx
+++ LifeCyle & UseEffect/src/components/Mounted.jsx
@@ -4,6 +4,10 @@ function ComponentDidMounted(){
 
     console.log("Mounted");
     useEffect(()=>{console.log("componentdidMount"); }, []);
+    useEffect(()=>{
+        const timer = setInterval(()=>console.log("tick"),1000);
+        return ()=>clearInterval(timer);
+    }, []);
     
 }
 
